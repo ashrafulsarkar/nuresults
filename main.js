254fbe8bd55f48f46a66364e9a34c9a91cd9ac12
Ashraful Sarkar <58996396+ashrafulsarkar@users.noreply.github.com>
2020-04-10 21:31:28 +0600

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
   Licensed under MIT (https://github.com/ashrafulsarkar/nuresult/blob/master/LICENSE)
 */
 
-document.querySelector("#year").addEventListener("change", function () {
+document.querySelector("#year").addEventListener("change", function myfunction() {
     year = document.querySelector("#year").value;
 });
 
@@ -15,27 +15,27 @@ function result_show() {
     var selectedValue = document.querySelector("#year").value;
     if (selectedValue == "select") {
         alert("Please Select Honours Year");
-        return;
+        return false;
     }
     if (year == "fourth" && roll_number == "" || year == "final" && roll_number == "") {
         alert("Please Enter Your Roll");
         document.getElementById('roll').focus();
-        return;
+        return false;
     }
     if (reg_no == "") {
         alert("Please Enter Your Registraion");
         document.getElementById('reg').focus();
-        return;
+        return false;
     }
     if (exam_year == "") {
         alert("Please Select Year");
         document.getElementById('exam_year').focus();
-        return;
+        return false;
     }
     if (letters_code == "") {
         alert("Please input Captcha Code");
         document.getElementById('letterCode').focus();
-        return;
+        return false;
     }
     var url = 'http://www.nu.ac.bd/results/honours/' + year + '_year_result_show.php?roll_number=' + roll_number + '&reg_no=' + reg_no + '&exam_year=' + exam_year + '&letters_code=' + letters_code;
     window.open(url, '_blank', 'height=750,width=780,left=130, top=5,resizable=yes, scrollbars');
